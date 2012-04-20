806fde3c02f0edc702372764cfe0599c613ffd2f
Taizo ITO <taizo@karesansui-project.info>
2012-04-20 15:51:18 +0900

    Changed copyright message.

--- karesansui/lib/collectd/countup.py
+++ karesansui/lib/collectd/countup.py
@@ -3,7 +3,7 @@
 #
 # This file is part of Karesansui.
 #
-# Copyright (C) 2012 HDE, Inc.
+# Copyright (C) 2009-2012 HDE, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
